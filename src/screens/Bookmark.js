7fe64c55fd12cf1a265a34a63627e1557571a806
Yannick COUSIN <94236356+yannick-cousin@users.noreply.github.com>
2022-01-05 18:28:40 +0100

    First update of Bookmark Screen

--- src/screens/Bookmark.js
+++ src/screens/Bookmark.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import axios from 'axios'
+import Gallery from '../components/Gallery'
 
-export default function Bookmark() {
-    return (
-        <div>
-            <h1>Bookmark</h1>
-        </div>
-    )
+import {useState, useEffect} from 'react'
+
+import '../components/styles/Bookmark.css'
+
+const Bookmark = ({isFavorite, setIsFavorite}) => {
+  
+	const [api, setApi] = useState([])
+	const [db, setDb] = useState([])
+
+	let temp = [];
+
+	const selectData = () => {
+		temp = [];
+		for (let i = 0; i < isFavorite.length; i++) {
+			for (let y = 0; y < api.length; y++) {
+				if (isFavorite[i] == api[y].id)
+				{
+					temp.push(api[y])
+				}
+			}
+		}
+		setDb(temp);
+	}
+
+	const getData = () => {
+		axios
+			.get("https://yannick-cousin.github.io/veille-api/api/all.json")
+			.then((response) => response.data)
+			.then((data) => setApi(data))
+	}
+
+	useEffect(() => {
+		getData()
+		selectData()
+	},[])
+
+	return (
+  	<div className="bookmark">
+    	<h1>Bookmark</h1>
+      <h3>Ici, vous retrouvez vos favoris sur cette page</h3>
+			<Gallery isFavorite={db} setIsFavorite={setIsFavorite} />
+    </div>
+  )
 }
+
+export default Bookmark
