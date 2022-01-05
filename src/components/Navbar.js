5475c26578e258ba0cda58451b54da19f16693b9
Yannick Lanteri <yannick-lanteri_student2021@wilder.school>
2022-01-05 15:13:48 +0100

    Create NavBar with icon and Nav bar Open and Close

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import "./styles/Navbar.css"
 
 export default function Navbar() {
     const [active, setActive] = React.useState(false);
@@ -7,17 +8,17 @@ export default function Navbar() {
     <nav className={`Nav ${active ? "NavOpen" : ""}`}> 
         <ion-icon onClick={() => setActive(!active)} name="chevron-forward-circle-sharp" id={active ? "ion-icon-open" : "ion-icon"}></ion-icon>      
         <NavLink className="NavLink" to="/">
-            <i class="fas fa-home fa-2x"></i>
+            <i class="fas fa-home fa-fw fa-2x"></i>
             <p className={`TextMenu ${active ? "" : "TextMenuOpen"}`}>Accueil</p>
         </NavLink>
         <NavLink className="NavLink" to="/bookmark">
-            <i className="fas fa-bookmark fa-2x"></i>
+            <i className="fas fa-bookmark fa-fw fa-2x"></i>
             <p className={`TextMenu ${active ? "" : "TextMenuOpen"}`}>Mes Favoris</p>
         </NavLink>
         <NavLink className="NavLink" to="/team">
-            <i className="fas fa-users fa-2x"></i>
+            <i className="fas fa-users fa-fw fa-2x"></i>
             <p className={`TextMenu ${active ? "" : "TextMenuOpen"}`}>Ma Promo</p>
         </NavLink>
-        <i id="NavDarkMode" className="far fa-lightbulb fa-2x"></i>
+        <i id="NavDarkMode" className="far fa-lightbulb fa-fw fa-2x"></i>
     </nav>
 );}
