b0f904d71c5ead9bc29cb93e68b441bd439691d7
Yannick COUSIN <94236356+yannick-cousin@users.noreply.github.com>
2022-01-07 15:54:42 +0100

    first commit
    Filter pas encore fonctionnel

--- src/screens/Bookmark.js
+++ src/screens/Bookmark.js
@@ -6,21 +6,34 @@ import '../components/styles/Bookmark.css'
 const Bookmark = ({ isFavorite, setIsFavorite }) => {
   const [api, setApi] = useState([]) //stock first data API
   const [db, setDb] = useState([]) // stock final data API
+  const [visibility, setVisibility] = useState(false) //
+  const [year, setYear] = useState([])
+  const [week, setWeek] = useState([])
+  const [selectWeek, setSelectWeek] = useState(0)
+  const [selectYear, setSelectYear] = useState(0)
   let temp = [] // stock temporaire
+  let tempWeek = [] //stock temporaire tri par date
+  let tempYear = [] //stock temporaire tri par date
 
   const selectData = () => {
     temp = []
     for (let i = 0; i < isFavorite.length; i++) {
-      console.log('phase 2', api.length, isFavorite)
       for (let y = 0; y < api.length; y++) {
         if (isFavorite[i] === api[y].id) {
-          console.log('chicken')
           temp.push(api[y])
+          /*console.log(api[y].week.indexOf(api[y].week))*/
+          if (tempWeek.indexOf(api[y].week) === -1) {
+            tempWeek.push(api[y].week)
+          }
+          if (tempYear.indexOf(api[y].year) === -1) {
+            tempYear.push(api[y].year)
+          }
         }
       }
-      console.log('poulet 04', temp)
     }
     setDb(temp)
+    setYear(tempYear)
+    setWeek(tempWeek)
   }
 
   useEffect(() => {
@@ -34,11 +47,67 @@ const Bookmark = ({ isFavorite, setIsFavorite }) => {
     selectData()
   }, [api])
 
+  const deroule = () => {
+    setVisibility(!visibility)
+  }
+
+  const selectDate = () => {
+    temp = []
+    for (let i = 0; i < db.length; i++) {
+      if (db[i].year === selectYear && db[i].week === selectWeek) {
+        temp.push(db[i])
+      }
+    }
+    setDb(temp)
+  }
+  console.log('setDb')
+  console.log(selectYear)
+  console.log(selectWeek)
+
   return (
     <div className='bookmark'>
-      {console.log('on est ici', api)}
       <h1>Bookmark</h1>
-      <h3>Ici, vous retrouvez vos favoris sur cette page</h3>
+      <div className='rowbutton'>
+        <div className='buttonselect'>
+          <i className='fas fa-search' onClick={() => deroule()} />
+          <form onSubmit={() => selectDate()}>
+            <div className={visibility ? 'year active' : 'cache'}>
+              <select name='year'>
+                {year.map(options => (
+                  <option
+                    onClick={e => setSelectYear(e.target.value)}
+                    value={selectYear}
+                  >
+                    {options}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className={visibility ? 'week active' : 'cache'}>
+              <select name='week'>
+                {week.map(options => (
+                  <option
+                    onClick={e => setSelectWeek(e.target.value)}
+                    value={selectWeek}
+                  >
+                    {options}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </form>
+          <i
+            className={visibility ? 'fas fa-arrow-circle-right' : 'cache'}
+            type='submit'
+            onClick={() => selectDate()}
+          />
+          <div className={visibility ? 'separateur' : 'cache'}> || </div>
+          <i
+            onClick={() => selectData()}
+            className={visibility ? 'fas fa-times-circle' : 'cache'}
+          />
+        </div>
+      </div>
       {db != [] && <Gallery isFavorite={db} setIsFavorite={setIsFavorite} />}
     </div>
   )
