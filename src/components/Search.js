4ceb05064df013413865966de38613d5c86de7c8
Pinchon Léo <leopinchonspa@gmail.com>
2022-01-14 11:33:06 +0100

    création du bouton search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './styles/Search.css'
-
+import search from '../assets/search.png'
 export default function Search() {
-    return (
-        <div className='searchBox'>
-            <div className='searchShadow'></div>
-            <input type="text" placeholder='Search'/>            
-        </div>
-    )
+  return (
+    <div className='searchBox'>
+      <div className='searchShadow'></div>
+      <input type='text' placeholder='Search' />
+      <img className='searchIcon' src={search} alt='seacrh' />
+    </div>
+  )
 }
