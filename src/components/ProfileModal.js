d25aace63cabf60f1219adda12d5543556d4275d
Dorian-Garcia-pro <garcia.dorian.pro@gmail.com>
2022-01-25 14:48:42 +0100

    maj

--- src/components/ProfileModal.js
+++ src/components/ProfileModal.js
@@ -1,9 +1,6 @@
 import React from "react";
-import imgDisco from "../assets/croix_rouge.png";
 import "./styles/ProfileModal.css";
 import { NavLink } from "react-router-dom";
-import { useState, useEffect } from "react";
-import axios from "axios";
 import LogoutIcon from "@mui/icons-material/Logout";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import BuildIcon from "@mui/icons-material/Build";
@@ -15,39 +12,47 @@ export default function ProfileModal({ active, setActive }) {
     location.reload();
   };
 
+  /*   window.addEventListener("click", function (e) {
+    if (document.getElementById("wrapperModalID").contains(e.target)) {
+      console.log("Clicked in");
+    } else {
+      console.log("Clicked out");
+    }
+  }); */
+
   return (
     <>
-      <div
-        className={`tricheCarre ${
-          active ? "tricheCarreHidden" : "tricheCarre"
-        }`}
-      >
-        <div className="petitcarre"></div>
-      </div>
-      <div
-        className={`bodyProfileModal ${active ? "bodyProfileModalHidden" : ""}`}
-      >
-        <NavLink to="/connect">
+      <div id="wrapperModalID" className="wrapperModal">
+        <div className={`tricheCarre ${active ? "" : "tricheCarreHidden"}`}>
+          <div className="petitcarre"></div>
+        </div>
+        <div
+          className={`bodyProfileModal ${
+            active ? "" : "bodyProfileModalHidden"
+          }`}
+        >
+          <NavLink to="/connect">
+            <div className="wrapDisconnect wrapOptionMenuProfile">
+              <button className="buttonOptionMenuProfile">
+                <AccountCircleIcon color="disabled" />
+                <label>Profil</label>
+              </button>
+            </div>
+          </NavLink>
+          <NavLink to="/connect">
+            <div className="wrapDisconnect wrapOptionMenuProfile">
+              <button className="buttonOptionMenuProfile">
+                <BuildIcon color="disabled" />
+                <label>Paramètres</label>
+              </button>
+            </div>
+          </NavLink>
           <div className="wrapDisconnect wrapOptionMenuProfile">
-            <button className="buttonOptionMenuProfile">
-              <AccountCircleIcon color="disabled" />
-              <label>Profile</label>
+            <button className="buttonOptionMenuProfile" onClick={disconnect}>
+              <LogoutIcon color="disabled" />
+              <label>Déconnexion</label>
             </button>
           </div>
-        </NavLink>
-        <NavLink to="/connect">
-          <div className="wrapDisconnect wrapOptionMenuProfile">
-            <button className="buttonOptionMenuProfile">
-              <BuildIcon color="disabled" />
-              <label>Paramétres</label>
-            </button>
-          </div>
-        </NavLink>
-        <div className="wrapDisconnect wrapOptionMenuProfile">
-          <button className="buttonOptionMenuProfile" onClick={disconnect}>
-            <LogoutIcon color="disabled" />
-            <label>Déconnexion</label>
-          </button>
         </div>
       </div>
     </>
