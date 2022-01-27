f9a086f1bc946057d76bb18ce09faa261ba18a40
Anouchka <anouchka.maurer@protonmail.com>
2022-01-27 15:32:34 +0100

    suppression de lignes commentées

--- src/components/ProfilArticles.js
+++ src/components/ProfilArticles.js
@@ -172,38 +172,5 @@ const ProfilArticles = (props) => {
     </div>
   )
 }
-//       <table className={!props.admin ? 'reveal' : 'cache'}>
-//         <thead className={!props.admin ? 'reveal' : 'cache'}>
-//           <tr className={!props.admin ? 'reveal' : 'cache'}>
-//             <th colspan='5'>Administration</th>
-//           </tr>
-//           <tr>
-//             <th>Semaine</th>
-//             <th>Année</th>
-//             <th>URL</th>
-//             <th>Description</th>
-//             <th>Supprimer</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {props.stokage.slice(0, props.moreArticle).map((detail) => (
-//             <tr>
-//               <th>{detail.week}</th>
-//               <th>{detail.year}</th>
-//               <th>{detail.url}</th>
-//               <th>{detail.description}</th>
-//               <th>
-//                 <i
-//                   onClick={() => props.deleteArticle(detail.id)}
-//                   class='fas fa-times-circle'
-//                 ></i>
-//               </th>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   )
-// }
 
 export default ProfilArticles
