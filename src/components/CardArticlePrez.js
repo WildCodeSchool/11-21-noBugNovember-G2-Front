c928b102e5bcf04515e44cc833c5f7356feeea0f
Martin Clara <clara-martin_student2021@wilder.school>
2022-01-27 12:43:34 +0100

    update Cardarticle

--- src/components/CardArticlePrez.js
+++ src/components/CardArticlePrez.js
@@ -1,87 +1,87 @@
-import axios from "axios";
-import check from "../assets/check.png";
-import FavoriteButton from "./FavoriteButton";
-import Im from "../assets/placeholder.jpg";
-import LikeButton from "./LikeButton";
-import { useEffect, useState } from "react";
-import "./styles/CardArticle.css";
+import axios from 'axios'
+import check from '../assets/check.png'
+import FavoriteButton from './FavoriteButton'
+import Im from '../assets/placeholder.jpg'
+import LikeButton from './LikeButton'
+import { useEffect, useState } from 'react'
+import './styles/CardArticle.css'
 
 export default function CardArticle(props) {
-  const [isReadMark, setIsReadMark] = useState(false);
+  const [isReadMark, setIsReadMark] = useState(false)
 
   const showCheck = () => {
-    props.setLink(props.url);
-    let temp = props.isRead;
+    props.setLink(props.url)
+    let temp = props.isRead
     if (!temp.includes(props.id)) {
-      temp.push(props.id);
-      props.changeIsRead(temp);
-      setIsReadMark(true);
+      temp.push(props.id)
+      props.changeIsRead(temp)
+      setIsReadMark(true)
     }
-  };
+  }
 
   useEffect(() => {
     if (props.isRead.includes(props.id)) {
-      setIsReadMark(true);
+      setIsReadMark(true)
     }
-  }, []);
+  }, [])
 
-  const [openGraph, setOpenGraph] = useState("");
+  const [openGraph, setOpenGraph] = useState('')
 
   useEffect(() => {
     axios
       .get(`http://localhost:3030/?url=${props.url}`)
       .then((response) => response.data)
-      .then((data) => setOpenGraph(data));
-  }, []);
+      .then((data) => setOpenGraph(data))
+  }, [])
 
   return (
     <>
-      <article className="card" key={props.id}>
-        <div className="cardContentTop" onClick={() => showCheck()}>
-          <div className="cardTopBanner">
-            <div className="cardContainerTopAvatar">
+      <article className='card' key={props.id}>
+        <div className='cardContentTop' onClick={() => showCheck()}>
+          <div className='cardTopBanner'>
+            <div className='cardContainerTopAvatar'>
               <img
-                className="cardTopAvatar"
+                className='cardTopAvatar'
                 src={props.avatar}
                 alt={props.member}
               />
             </div>
-            <div className="cardTopTitle">
-              <p className="cardTopTitleP" maxLength="10">
+            <div className='cardTopTitle'>
+              <p className='cardTopTitleP' maxLength='10'>
                 {props.name}
               </p>
             </div>
-            <div className="cardDate">
+            <div className='cardDate'>
               <p>
                 S{props.week} - {props.year}
               </p>
-              <p className="cardMember">{props.member}</p>
+              <p className='cardMember'>{props.member}</p>
             </div>
           </div>
-          <div className="cardImgBox">
+          <div className='cardImgBox'>
             <img
-              className={openGraph.image ? "cardImg" : "cardImgPlaceholder"}
+              className={openGraph.image ? 'cardImg' : 'cardImgPlaceholder'}
               src={openGraph.image ? openGraph.image : Im}
-              alt=""
+              alt=''
             />
             <img
-              className={isReadMark ? "checkMarkIcon" : ""}
-              src={isReadMark ? check : ""}
-              alt=""
+              className={isReadMark ? 'checkMarkIcon' : ''}
+              src={isReadMark ? check : ''}
+              alt=''
             />
           </div>
         </div>
 
-        <div className="cardBottom">
-          <div className="cardBottomDescritption">
+        <div className='cardBottom'>
+          <div className='cardBottomDescritption'>
             <p>{openGraph.title ? openGraph.title : props.description}</p>
           </div>
 
-          <div className="cardBottomFooter">
+          <div className='cardBottomFooter'>
             <a
-              className="cardBottomLink"
-              target="_blank"
-              rel="noreferrer"
+              className='cardBottomLink'
+              target='_blank'
+              rel='noreferrer'
               onClick={() => props.clickOpenPartage(props.url)}
             >
               ⛬
@@ -96,5 +96,5 @@ export default function CardArticle(props) {
         </div>
       </article>
     </>
-  );
+  )
 }
